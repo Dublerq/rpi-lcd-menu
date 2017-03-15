23f51296bc087b1e2a05a6dee0aa460ba1562f4e
Dubler <dublerscontact@gmail.com>
2017-03-15 20:08:29 +0000

    Filling first files with first, placeholder functions

---
+++ examples/example.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+from rpilcdmenu import *
+from rpilcdmenu.items import *
+
+def main():
+	menu = RpiLCDMenu()
+	menu.displayTestScreen()
+
+if __name__ == "__main__":
+    main()
---
+++ rpilcdmenu/rpi_lcd_menu.py
@@ -0,0 +1,17 @@
+class RpiLCDMenu(object):
+
+	def __init__(self):
+		"""
+		TODO
+		"""
+
+	def _clearDisplay(self):	
+		"""
+		Clear LCD Screen
+		"""
+
+	def displayTestScreen(self):
+		"""
+		Display test screen to see if your LCD screen is wokring
+		"""
+		print("displaying test screen...")
